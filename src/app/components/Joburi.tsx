968a577e78bc2d857357147bc7fee2aa83178eba
Pop Sergiu <sergiugeorgepop@gmail.com>
2024-05-24 18:51:13 +0300

    fix: Added the NoResults.tsx component Modified Joburi.tsx to check if there are no results, and display the component. Has basic responsiveness for screens above 768px.

--- src/app/components/Joburi.tsx
+++ src/app/components/Joburi.tsx
@@ -1,5 +1,6 @@
 import JobCard from "./JobCard";
 import { JobsResults } from "@/models/Jobs";
+import FaraRezultate from "./NoResults";
 
 type Props = {
   data: JobsResults | undefined;
@@ -9,8 +10,8 @@ const Joburi = async ({ data }: Props) => {
     (data?.numFound ?? 0) >= 20
       ? "de rezultate"
       : (data?.numFound ?? 0) == 1
-      ? "rezultat"
-      : "rezultate";
+        ? "rezultat"
+        : "rezultate";
 
   // if (numFound === 0) return <h2>No Jobs</h2>;
 
@@ -19,7 +20,8 @@ const Joburi = async ({ data }: Props) => {
       <h2 className="ml-8 text-center md:text-start text-text_grey_darker my-8 text-lg">
         {data?.numFound} {nrJoburi}
       </h2>
-
+      {/* Check if there are no results and render the NoResults component */}
+      {data?.numFound === 0 ? <FaraRezultate /> : null}
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-7">
         {data?.docs.map((item, index) => (
           <JobCard item={item} key={index} />
@@ -29,7 +31,7 @@ const Joburi = async ({ data }: Props) => {
       {/* {numFound === docs.length ? null : (
           <button onClick={handleLoadMore}>Load More Jobs</button>
         )} */}
-    </main>
+    </main >
   );
 };
 
