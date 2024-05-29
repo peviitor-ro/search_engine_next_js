fb711dfa14c9e7fc4c076dd5af24858df537c921
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2024-05-29 10:04:34 +0300

    add pagination ui

--- src/app/components/Joburi.tsx
+++ src/app/components/Joburi.tsx
@@ -10,10 +10,8 @@ const Joburi = async ({ data }: Props) => {
     (data?.numFound ?? 0) >= 20
       ? "de rezultate"
       : (data?.numFound ?? 0) == 1
-        ? "rezultat"
-        : "rezultate";
-
-  // if (numFound === 0) return <h2>No Jobs</h2>;
+      ? "rezultat"
+      : "rezultate";
 
   return (
     <main className="mb-auto">
@@ -21,17 +19,16 @@ const Joburi = async ({ data }: Props) => {
         {data?.numFound} {nrJoburi}
       </h2>
       {/* Check if there are no results and render the NoResults component */}
-      {data?.numFound === 0 ? <FaraRezultate /> :
+      {data?.numFound === 0 ? (
+        <FaraRezultate />
+      ) : (
         <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-7">
           {data?.docs.map((item, index) => (
             <JobCard item={item} key={index} />
           ))}
         </div>
-      }
-      {/* {numFound === docs.length ? null : (
-          <button onClick={handleLoadMore}>Load More Jobs</button>
-        )} */}
-    </main >
+      )}
+    </main>
   );
 };
 
