af64ffc6fcf588e5b2f0aa7ad3e176a37e745aba
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2024-07-05 16:57:30 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Exclude pages with getServerSideProps from static export
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    // Example: Exclude "/rezultate" page from static export
+    delete defaultPathMap["/rezultate"];
+    // Return the modified path map
+    return defaultPathMap;
+  },
+};
 
 export default nextConfig;
