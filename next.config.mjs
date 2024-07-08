13c3cf19439bd18598a84345b3407596f3f8b5ef
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2024-07-08 11:20:58 +0300

    change to generateStaticParams

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    let pathsToExport = { ...defaultPathMap };
-
-    // Remove paths that use getServerSideProps
-    delete pathsToExport["/rezultate"];
-    // Add other pages as necessary
-
-    return pathsToExport;
+  generateStaticParams() {
+    // Generate static parameters for pages here
+    return {
+      "/": {
+        /* Static parameters for root */
+      },
+    };
   },
 };
 
