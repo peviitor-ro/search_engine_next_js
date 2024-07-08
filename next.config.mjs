522de977c8ae5cd4bf8ac50858d739cd4e27aae8
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2024-07-08 11:13:26 +0300

    change deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    let pathsToExport = { ...defaultPathMap };
+
+    // Remove paths that use getServerSideProps
+    delete pathsToExport["/rezultate"];
+    // Add other pages as necessary
+
+    return pathsToExport;
+  },
+};
 
 export default nextConfig;
