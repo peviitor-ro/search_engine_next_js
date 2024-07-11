d01d34f98996c5e1049aa9d85b6b397779aee726
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2024-07-11 16:09:02 +0300

    remove comments

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -23,7 +23,7 @@ export default function FaraPagina() {
           fetchPriority="auto"
         />
       </div>
-      {/* <CountdownButton /> */}
+      <CountdownButton />
       <Footer />
     </div>
   );
